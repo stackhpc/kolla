bebe25959e12a7a0bc5e7a2c95dbed85642ef949
Isaac Prior <isaac@stackhpc.com>
2019-08-06 12:09:13 +0100

    Ensures monasca-thresh data dir exists and is empty on start up
    This prevents the data directory from filling up with Apache Storm
    temporary files which will otherwise not be cleared.
    
    Change-Id: Ib07e32f4e67e500f10986103d781dfd3874ffdd2

--- docker/monasca/monasca-thresh/extend_start.sh
+++ docker/monasca/monasca-thresh/extend_start.sh
@@ -1,15 +1,29 @@
 #!/bin/bash
 
-# Create log directory, with appropriate permissions
+# Create log and data directories, with appropriate permissions
 MONASCA_LOG_DIR="/var/log/kolla/monasca"
+MONASCA_DATA_DIR="/var/lib/monasca-thresh/data"
 if [[ ! -d "$MONASCA_LOG_DIR" ]]; then
     mkdir -p $MONASCA_LOG_DIR
+fi
+if [[ ! -d "$MONASCA_DATA_DIR" ]]; then
+    mkdir -p $MONASCA_DATA_DIR
 fi
 if [[ $(stat -c %U:%G ${MONASCA_LOG_DIR}) != "monasca:kolla" ]]; then
     chown monasca:kolla ${MONASCA_LOG_DIR}
 fi
+if [[ $(stat -c %U:%G ${MONASCA_DATA_DIR}) != "monasca:kolla" ]]; then
+    chown monasca:kolla ${MONASCA_DATA_DIR}
+fi
 if [[ $(stat -c %a ${MONASCA_LOG_DIR}) != "755" ]]; then
     chmod 755 ${MONASCA_LOG_DIR}
+fi
+if [[ $(stat -c %a ${MONASCA_DATA_DIR}) != "755" ]]; then
+    chmod 755 ${MONASCA_DATA_DIR}
+fi
+
+if [[ $(ls -Ab ${MONASCA_DATA_DIR}) != "" ]]; then
+    find ${MONASCA_DATA_DIR} -mindepth 1 -delete
 fi
 
 . /usr/local/bin/kolla_monasca_extend_start
